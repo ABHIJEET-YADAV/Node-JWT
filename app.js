419620c4ee486a54cd2720143c09cdd3e6d64bcc
ABHIJEET-YADAV <abhijeet1996yadav@gmail.com>
2018-03-31 23:22:41 +0530

    api

--- app.js
+++ app.js
@@ -36,12 +36,19 @@ app.post('/api/login', function(req, res){
   });
 });
 
+// Verify Token
 function verifyToken(req, res, next) {
+  // Get auth header value
   const bearerHeader = req.headers['authorization'];
+  // Check if bearer is undefined
   if(typeof bearerHeader !== 'undefined') {
+    // Split at the space
     const bearer = bearerHeader.split(' ');
+    // Get token from array
     const bearerToken = bearer[1];
+    // Set the token
     req.token = bearerToken;
+    // Next middleware
     next();
   } else {
     console.log("Access")
